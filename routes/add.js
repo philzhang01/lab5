a6e2a83496b5cef18b61d397100e376a1de8bf4e
philzhang01 <philzhang01@gmail.com>
2020-02-05 22:58:16 -0800

    finish lab5

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,13 @@
 var data = require("../data.json");
 
-exports.addFriend = function(request, response) {    
-	// Your code goes here
- }
+exports.addFriend = function(request, response) {
+  // Your code goes here
+  var name = request.query.name;
+  var desc = request.query.description;
+  var newFriend = {
+    name: name,
+    description: desc,
+    imageURL: "http://lorempixel.com/400/400/people/"
+  };
+  data.friends.push(newFriend);
+};
